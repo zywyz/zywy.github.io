243108acd0ed8631a9784e51c1d5cc9b357a7318
Filip <filip.zywicki@gmail.com>
2020-01-28 11:59:02 +0100

    add deploy command

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          zywyz.github.io
+          zywyz.github.io/zywy
         </p>
       </header>
     </div>
